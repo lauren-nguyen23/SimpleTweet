8a173ab70c3d2dc28f8f8b21ee903adb17ec3d3b
lauren-nguyen23 <tranghanguyen242@gmail.com>
2022-03-13 23:44:38 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.kt
@@ -0,0 +1,19 @@
+package com.codepath.apps.restclienttemplate.models
+
+import org.json.JSONObject
+
+class User {
+    var name: String = ""
+    var screenName: String = ""
+    var profileImageUrl: String = ""
+
+    companion object {
+        fun fromJson (jsonObject: JSONObject): User {
+            val user = User()
+            user.name = jsonObject.getString("name")
+            user.screenName = jsonObject.getString("screen_name")
+            user.profileImageUrl = jsonObject.getString("profile_image_url_https")
+            return user
+        }
+    }
+}
