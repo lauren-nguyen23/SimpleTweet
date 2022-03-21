cfd2e4f2181da3ae11db05bf112355e3574e5006
lauren-nguyen23 <tranghanguyen242@gmail.com>
2022-03-21 01:22:39 -0400

    Update function: publish a Tweet to user timeline

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -0,0 +1,103 @@
+package com.codepath.apps.restclienttemplate
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.codepath.apps.restclienttemplate.models.Tweet
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
+
+class ComposeActivity : AppCompatActivity() {
+
+    lateinit var etCompose:EditText
+    lateinit var btnTweet: Button
+    lateinit var tvCharacterCount: TextView
+
+    lateinit var client: TwitterClient
+
+    var charsRemaining:Int = 280
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compose)
+
+        etCompose = findViewById(R.id.etTweetCompose)
+        btnTweet = findViewById(R.id.buttonTweet)
+        tvCharacterCount = findViewById(R.id.tvCharacterCount)
+
+        client = TwitterApplication.getRestClient(this)
+
+        //Handling the user's click on the tweet button
+        btnTweet.setOnClickListener {
+            //Grab the content of the edittext (etCompose)
+            val tweetContent = etCompose.text.toString()
+
+            //1. Make sure the tweet isn't empty
+            if (tweetContent.isEmpty()) {
+                Toast.makeText(this, "Empty tweets not allowed!", Toast.LENGTH_SHORT).show()
+                //Look into displaying SnackBar message
+            }
+            else {
+                //2. Make sure the tweet is under character count
+                if (tweetContent.length >280) {
+                    Toast.makeText(this, "Tweet is too long! Limit is 280 characters", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    client.publishTweet(tweetContent, object: JsonHttpResponseHandler() {
+                        override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                            Log.i(TAG, "Successfully published tweet!")
+
+                            val tweet = Tweet.fromJson(json.jsonObject)
+
+                            val intent = Intent()
+                            intent.putExtra("tweet", tweet)
+                            setResult(RESULT_OK, intent)
+                            finish()
+                        }
+
+                        override fun onFailure(
+                            statusCode: Int,
+                            headers: Headers?,
+                            response: String?,
+                            throwable: Throwable?
+                        ) {
+                            Log.e(TAG, "Failed to publish tweet", throwable)
+                        }
+                    })
+                }
+            }
+        }
+
+        etCompose.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                // Fires right as the text is being changed (even supplies the range of text)
+                var count:Int = etCompose.getText().toString().length
+                tvCharacterCount.setText((280-count).toString())
+                if (count > 280) {
+                    btnTweet.isEnabled = false
+                    tvCharacterCount.setTextColor(Color.RED)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Fires right before text is changing
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // Fires right after the text has changed
+            }
+        })
+
+    }
+    companion object {
+        val TAG = "ComposeActivity"
+    }
+}
